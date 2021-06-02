8f91298bfa4c1f6b9d92fe038fb9061cede2a17c
Ederson S <edersonrodrigo31@gmail.com>
2021-06-02 11:37:42 -0300

    [FEAT]: ensure createUser Controller returns a sucess message

--- src/modules/user/controllers/createUser.ts
+++ src/modules/user/controllers/createUser.ts
@@ -1,5 +1,11 @@
+export type HttpResponse = {
+  statusCode: number
+  body?: any
+}
+
 export class createUserController {
-  async handle(request: any): Promise<any> {
-    return request
+  async handle(request: any): Promise<HttpResponse> {
+    return { statusCode: 201, body: {"success": true,
+    "message": "User successfully registered!"}}
   }
 }
