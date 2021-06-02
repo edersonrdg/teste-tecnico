15f316a6b3ed1cf67b1f00d427ac66f87f3b9184
Ederson S <edersonrodrigo31@gmail.com>
2021-06-02 18:26:44 -0300

    [TEST]: ensure create user validate returns error if no lastname is provided

--- __tests__/unit/validators/createUserValidate.spec.ts
+++ __tests__/unit/validators/createUserValidate.spec.ts
@@ -14,12 +14,28 @@ describe('[VALIDATION] - create user', () => {
     const request = {
       lastname: "Doe",
       phone: "+55119988023212",
+      cpf: "12312312312"
     }
     try {
       sut.validate(request)
     } catch (error) {
       expect(error instanceof BaseError).toBe(true)
       expect(error.message).toBe("\"name\" is required")
+      expect(error.statusCode).toBe(400)
+    }
+  })
+  it('Should return error if no user lastname is provided', async () => {
+    const { sut } = makeSut()
+    const request = {
+      name: "Doe",
+      phone: "+55119988023212",
+      cpf: "12312312312"
+    }
+    try {
+      sut.validate(request)
+    } catch (error) {
+      expect(error instanceof BaseError).toBe(true)
+      expect(error.message).toBe("\"lastname\" is required")
       expect(error.statusCode).toBe(400)
     }
   })
