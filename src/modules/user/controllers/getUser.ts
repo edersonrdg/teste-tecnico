52a390cfec1388c7849c010426df157440b92a42
Ederson S <edersonrodrigo31@gmail.com>
2021-06-04 10:16:44 -0300

    [INFRA]: get user data interface

--- src/modules/user/controllers/getUser.ts
+++ src/modules/user/controllers/getUser.ts
@@ -3,14 +3,18 @@ import { errorTreatment, sucess } from "@shared/helpers/http-helper";
 import { HttpResponse } from "@shared/protocols/http";
 import { UseCase } from "@shared/protocols/useCase";
 
+interface User {
+  userId: string
+}
+
 export class GetUserController implements Controller {
   constructor (
     private readonly getUser: UseCase,
   ) {}
 
-  async handle(userId: string): Promise<HttpResponse> {
+  async handle(data: User): Promise<HttpResponse> {
     try {
-      const response = await this.getUser.execute(userId)
+      const response = await this.getUser.execute(data.userId)
       return sucess(response)
     } catch (error) {
       return errorTreatment(error)
